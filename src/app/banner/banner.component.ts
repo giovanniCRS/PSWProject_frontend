b27b8843e8a0b73d8ff50fb197ce5ae5a03ddbdd
giovanniCRS <gio.caruso26@gmail.com>
2025-02-22 13:59:19 +0100

    Abbellimenti

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements AfterViewInit {
+  @ViewChild('bgVideo') video!: ElementRef;
 
+  ngAfterViewInit() {
+    if (this.video) {
+      const videoElement = this.video.nativeElement;
+      videoElement.src = './assets/banner-video.mp4';
+      videoElement.load();
+      videoElement.muted = true; // Assicura la riproduzione su alcuni browser
+      videoElement.play().catch((err: any) => {
+        console.error("Errore nella riproduzione del video:", err);
+      });
+    } else {
+      console.error("Elemento video non trovato!");
+    }
+  }
 }
